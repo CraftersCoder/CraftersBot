25788f7da64f2f2872c4607fa78ea5e7ab4afdb2
CraftersCoder <126096212+CraftersCoder@users.noreply.github.com>
2023-02-22 17:15:40 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, Collection, Partials, GatewayIntentBits, ActivityType, Events } = require ('discord.js');
+const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
+const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
+
+const client = new Client({
+    intents: [ Guilds, GuildMembers, GuildMessages ],
+    partials: [ User, Message, GuildMember, ThreadMember ]
+})
+
+client.once("ready", () => {
+    console.log(`${client.user.tag} est connecté !`);
+    client.user.setPresence({ activities: [{ name: `la version ${require(`${process.cwd()}/package.json`).version}`, type: ActivityType.Watching }], status: 'idle' })
+})
+
+client.config = require('./config.json');
+
+client.login(client.config.BOT.TOKEN)
