39a0b9476a76b2163502892613e8f848fe06bffd
CraftersCoder <126096212+CraftersCoder@users.noreply.github.com>
2023-02-22 17:29:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
+// Importation des dépendances
 const { Client, Collection, Partials, GatewayIntentBits, ActivityType, Events } = require ('discord.js');
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember, Channel } = Partials;
 
+// Création du client Discord.js
 const client = new Client({
-    intents: [ Guilds, GuildMembers, GuildMessages ],
-    partials: [ User, Message, GuildMember, ThreadMember ]
-})
+    intents: [ Guilds, GuildMembers, GuildMessages ], // Activation des intentions
+    partials: [ User, Message, GuildMember, ThreadMember ] // Activation des partials
+});
 
+// Code exécuté une fois le bot prêt
 client.once("ready", () => {
     console.log(`${client.user.tag} est connecté !`);
-    client.user.setPresence({ activities: [{ name: `la version ${require(`${process.cwd()}/package.json`).version}`, type: ActivityType.Watching }], status: 'idle' })
-})
+    // Mise à jour de la présence du bot avec la version du package.json
+    client.user.setPresence({ activities: [{ name: `la version ${require(`${process.cwd()}/package.json`).version}`, type: ActivityType.Watching }], status: 'idle' });
+});
 
+// Chargement de la configuration depuis un fichier externe
 client.config = require('./config.js');
 
-client.login(client.config.BOT.TOKEN)
+// Connexion du bot à Discord avec le token d'authentification
+client.login(client.config.BOT.TOKEN);
